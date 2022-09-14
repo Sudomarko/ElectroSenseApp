37025fec53db4ee92b770678478d5bb94335dffc
Sudomarko <cigamali007@gmail.com>
2022-09-14 12:02:46 -0400

    Adding project

---
+++ Shared/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Marko Vukasinovic on 9/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("ElectroSense")
+            .font(.title)
+            .foregroundColor(Color.blue)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/ElectroSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectroSenseApp.swift
+//  Shared
+//
+//  Created by Marko Vukasinovic on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ElectroSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
