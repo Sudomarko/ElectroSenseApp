7078ebd651571492b229b63ffbafe193b201f0f0
Sudomarko <cigamali007@gmail.com>
2022-09-23 14:56:05 -0400

    Adding menu buttons (no formatting of any kind)

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,11 +8,101 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert = false
     var body: some View {
-        Text("ElectroSense")
-            .font(.title)
-            .foregroundColor(Color.blue)
-            .padding()
+        VStack {
+            HStack {
+                Button {
+                    showAlert = true
+                } label: {
+                    Text("Circulation")
+                        .font(.title)
+                        .foregroundColor(Color.blue)
+                        .padding()
+                }.alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("Circulation")
+                        
+                    )
+                }
+                
+                Button {
+                    showAlert = true
+                } label: {
+                    Text("Heart Rate")
+                        .font(.title)
+                        .foregroundColor(Color.blue)
+                        .padding()
+                }.alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("Heart Rate")
+                        
+                    )
+                }
+            }
+            
+            HStack {
+                Button {
+                    showAlert = true
+                } label: {
+                    Text("Puls Ox")
+                        .font(.title)
+                        .foregroundColor(Color.blue)
+                        .padding()
+                }.alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("Puls Ox")
+                        
+                    )
+                }
+                
+                Button {
+                    showAlert = true
+                } label: {
+                    Text("Body Pressure")
+                        .font(.title)
+                        .foregroundColor(Color.blue)
+                        .padding()
+                }.alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("Body Pressure")
+                        
+                    )
+                }
+            }
+            
+            HStack {
+                Button {
+                    showAlert = true
+                } label: {
+                    Text("Temperature")
+                        .font(.title)
+                        .foregroundColor(Color.blue)
+                        .padding()
+                }.alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("Temperature")
+                        
+                    )
+                }
+                
+                Button {
+                    showAlert = true
+                } label: {
+                    Text("ElectroSense")
+                        .font(.title)
+                        .foregroundColor(Color.blue)
+                        .padding()
+                }.alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("ElectroSense")
+                        
+                    )
+                }
+            }
+            
+        }
+        
     }
 }
 
