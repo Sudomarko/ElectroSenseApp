544f16893c3b585fad2594f40b3b1ece72448ae5
Sudomarko <cigamali007@gmail.com>
2022-09-30 12:50:28 -0400

    Added different views

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,97 +10,181 @@ import SwiftUI
 struct ContentView: View {
     @State private var showAlert = false
     var body: some View {
-        VStack {
-            HStack {
-                Button {
-                    showAlert = true
-                } label: {
-                    Text("Circulation")
-                        .font(.title)
-                        .foregroundColor(Color.blue)
-                        .padding()
-                }.alert(isPresented: $showAlert) {
-                    Alert(
-                        title: Text("Circulation")
-                        
-                    )
-                }
-                
-                Button {
-                    showAlert = true
-                } label: {
-                    Text("Heart Rate")
-                        .font(.title)
-                        .foregroundColor(Color.blue)
-                        .padding()
-                }.alert(isPresented: $showAlert) {
-                    Alert(
-                        title: Text("Heart Rate")
-                        
-                    )
-                }
-            }
-            
-            HStack {
-                Button {
-                    showAlert = true
-                } label: {
-                    Text("Puls Ox")
-                        .font(.title)
-                        .foregroundColor(Color.blue)
-                        .padding()
-                }.alert(isPresented: $showAlert) {
-                    Alert(
-                        title: Text("Puls Ox")
-                        
-                    )
-                }
-                
-                Button {
-                    showAlert = true
-                } label: {
-                    Text("Body Pressure")
-                        .font(.title)
-                        .foregroundColor(Color.blue)
-                        .padding()
-                }.alert(isPresented: $showAlert) {
-                    Alert(
-                        title: Text("Body Pressure")
-                        
-                    )
+        NavigationView {
+            VStack {
+                HStack {
+                    NavigationLink(destination: CirculationView()) {
+                        Text("Circulation")
+                            .font(.title)
+                            .foregroundColor(Color.blue)
+                            .padding()
+                    }
+                    
+                    NavigationLink(destination: HeartRateView()) {
+                        Text("Heart Rate")
+                            .font(.title)
+                            .foregroundColor(Color.blue)
+                            .padding()
+                    }
+                    
+                }.offset(x:0, y:-100)
+                HStack {
+                    NavigationLink(destination: PulseOxView()) {
+                        Text("Pulse Ox")
+                            .font(.title)
+                            .foregroundColor(Color.blue)
+                            .padding()
+                    }
+                    
+                    NavigationLink(destination: BodyPressureView()) {
+                        Text("Body Pressure")
+                            .font(.title)
+                            .foregroundColor(Color.blue)
+                            .padding()
+                    }
+                    
+                }.offset(x:0, y:-50)
+                HStack {
+                    NavigationLink(destination: TemperatureView()) {
+                        Text("Temperature")
+                            .font(.title)
+                            .foregroundColor(Color.blue)
+                            .padding()
+                    }
+                    
+                    NavigationLink(destination: ElectroSenseView()) {
+                        Text("ElectroSense")
+                            .font(.title)
+                            .foregroundColor(Color.blue)
+                            .padding()
+                    }
+                    
                 }
             }
-            
-            HStack {
-                Button {
-                    showAlert = true
-                } label: {
-                    Text("Temperature")
-                        .font(.title)
-                        .foregroundColor(Color.blue)
-                        .padding()
-                }.alert(isPresented: $showAlert) {
-                    Alert(
-                        title: Text("Temperature")
-                        
-                    )
-                }
-                
-                Button {
-                    showAlert = true
-                } label: {
-                    Text("ElectroSense")
-                        .font(.title)
-                        .foregroundColor(Color.blue)
-                        .padding()
-                }.alert(isPresented: $showAlert) {
-                    Alert(
-                        title: Text("ElectroSense")
-                        
-                    )
-                }
+        }
+    }
+}
+struct CirculationView: View {
+    @State private var showAlert = false
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Data Placeholder")
             }
-            
+            .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        VStack {
+                            Text("Circulation").font(.largeTitle)
+                            Text("Subtitle").font(.subheadline)
+                        }
+                    }
+                }
+        }
+        
+    }
+}
+
+struct HeartRateView: View {
+    @State private var showAlert = false
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Data Placeholder")
+            }
+            .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        VStack {
+                            Text("Heart Rate").font(.largeTitle)
+                            Text("Subtitle").font(.subheadline)
+                        }
+                    }
+                }
+        }
+        
+    }
+}
+
+struct PulseOxView: View {
+    @State private var showAlert = false
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Data Placeholder")
+            }
+            .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        VStack {
+                            Text("Pulse Ox").font(.largeTitle)
+                            Text("Subtitle").font(.subheadline)
+                        }
+                    }
+                }
+        }
+        
+    }
+}
+
+struct BodyPressureView: View {
+    @State private var showAlert = false
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Data Placeholder")
+            }
+            .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        VStack {
+                            Text("Body Pressure").font(.largeTitle)
+                            Text("Subtitle").font(.subheadline)
+                        }
+                    }
+                }
+        }
+        
+    }
+}
+
+struct TemperatureView: View {
+    @State private var showAlert = false
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Data Placeholder")
+            }
+            .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        VStack {
+                            Text("Temperature").font(.largeTitle)
+                            Text("Subtitle").font(.subheadline)
+                        }
+                    }
+                }
+        }
+        
+    }
+}
+
+struct ElectroSenseView: View {
+    @State private var showAlert = false
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Data Placeholder")
+            }
+            .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        VStack {
+                            Text("ElectroSense").font(.largeTitle)
+                            Text("Subtitle").font(.subheadline)
+                        }
+                    }
+                }
         }
         
     }
